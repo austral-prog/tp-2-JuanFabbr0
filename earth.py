a7d4a6d6d68afe746bb697b6bf1f3f6fec5f72a1
JuanFabbr0 <jfabbro@mail.austral.edu.ar>
2025-04-01 14:09:52 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -5,4 +5,3 @@ def earth():
 	falso = y < x
 	print(f'The result of {x} comes first in the dictionary than {y} is {verdad}')
 	print(f' The result of {y} comes first in the dictionary than {x} is {falso}')
-	
